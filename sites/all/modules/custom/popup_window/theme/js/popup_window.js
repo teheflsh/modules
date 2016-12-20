71277a90c9b724733fc5cbaf3ef5739e945be203
Unknown <teheflsh@yandex.ru>
2016-12-20 16:50:43 +0600

    Upload new custom module 'PopUp Window'

---
+++ sites/all/modules/custom/popup_window/theme/js/popup_window.js
@@ -0,0 +1,65 @@
+(function ($) {
+  Drupal.behaviors.popup_window = {
+    attach: function (context, settings) {
+      var posX;
+      var posY;
+
+      $('.popup-bar', context).bind('mousedown', mouseDown, false);
+      $(window, context).bind('mouseup', mouseUp, false);
+
+      $(document, context).ready(function () {
+        if (settings.popup_window.message) {
+          $('#overlay', context).show();
+          $('#popup', context).show();
+          $('#popup', context).css('min-height', settings.popup_window.height + '.px');
+          $('#popup', context).css('width', settings.popup_window.width + '.px');
+          $('#popup', context).css('top', ($(window).height() / 2 - $('#popup').outerHeight() / 2));
+          $('#popup', context).css('left', $(window).width() / 2 - $('#popup').outerWidth() / 2);
+          $('body', context).css('overflow', 'hidden');
+        }
+      });
+
+      $('#overlay', context).click(function () {
+        close();
+      });
+
+      $('.btn-close', context).click(function () {
+        close();
+      });
+
+      function close() {
+        $('#overlay', context).hide();
+        $('#popup', context).hide();
+        $('body', context).css('overflow', 'auto');
+      }
+
+      function mouseDown(e) {
+        var offset = $('#popup', context).offset();
+        posX = e.clientX - offset.left;
+        posY = e.clientY - offset.top;
+        $(window, context).bind('mousemove', popupMove, true);
+
+      };
+
+      function mouseUp() {
+        $(window, context).unbind('mousemove', popupMove, true);
+      };
+
+      function popupMove(e) {
+        var top = e.clientY - posY;
+        var left = e.clientX - posX;
+        var borderTop = 0;
+        var borderBottom = $(window).height() - $('#popup').outerHeight();
+        var borderLeft = 0;
+        var borderRight = $(window).width() - $('#popup').outerWidth();
+        if (top >= borderTop && top <= borderBottom) {
+          $('#popup', context).css('top', top);
+        }
+
+        if (left >= borderLeft && left <= borderRight) {
+          $('#popup', context).css('left', left);
+        }
+      }
+    },
+  };
+}(jQuery));
---
+++ sites/all/modules/custom/popup_window/theme/tpl/popup_window.tpl.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * Default template for popup_window.
+ */
+?>
+<div id="overlay"></div>
+<div id="popup">
+  <div class="popup-bar">Status Messages<span class="btn-close">[X]</span></div>
+  <div class="popup-content">
+    <?php
+    $output = '';
+
+    foreach ($status_messages as $type => $messages) {
+      $output .= "<div class=\"messages $type\">\n";
+      if (!empty($status_heading[$type])) {
+        $output .= '<h2 class="element-invisible">' . $status_heading[$type] . "</h2>\n";
+      }
+      if (count($messages) > 1) {
+        $output .= " <ul>\n";
+        foreach ($messages as $message) {
+          $output .= '  <li>' . $message . "</li>\n";
+        }
+        $output .= " </ul>\n";
+      }
+      else {
+        $output .= reset($messages);
+      }
+      $output .= "</div>\n";
+    }
+
+    echo $output;
+    ?>
+  </div>
+</div>
